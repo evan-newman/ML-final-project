997545e1a55aa40cdebd42f7d46ef5f3b1b0bea7
evanrnewman <newmanrevan@gmail.com>
2019-06-12 11:27:59 -0700

    Decided on BalancedRandomForestClassifier and DT. Added files for training on 103 features and all features along with adding in the predicted values on the test data to a file

---
+++ features103_pred3.py
@@ -0,0 +1,77 @@
+import numpy as np
+from imblearn.over_sampling import SMOTE
+from sklearn import model_selection
+from sklearn import metrics
+from imblearn.ensemble import BalancedRandomForestClassifier 
+
+def main():
+    f = open("trainingData/feature103_Train.txt")
+
+    data = []
+    label = []
+    for lineNumber, line in enumerate(f):
+        if lineNumber != 0:
+            newLine = line.rstrip()
+            entries = newLine.split('\t')
+            data.append(list(map(float, entries[2:])))
+            label.append(int(entries[1]))
+
+    f.close()
+
+    f2 = open("testingData/features103_test.txt")
+
+    extractTest = []
+    extractIds = []
+    for lineNumber, line in enumerate(f2):
+        if lineNumber != 0:
+            newLine = line.rstrip()
+            entries = newLine.split('\t')
+            extractTest.append(list(map(float, entries[1:])))
+            extractIds.append(entries[0])
+
+    f2.close()
+
+
+    trainLabel = np.array(label)
+    trainData = np.array(data)
+
+    testData = np.asarray(extractTest)
+
+    ###########################################################
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(trainData, trainLabel, test_size = 0.33)
+
+    X_train, y_train = SMOTE().fit_resample(X_train, y_train)
+
+    clf_train = BalancedRandomForestClassifier(n_estimators=100, max_depth=5)
+    clf_train = clf_train.fit(X_train, y_train)
+
+    y_dt_pred_train = clf_train.predict_proba(X_test)
+
+    onlyPKpredictions_train = y_dt_pred_train[:,1]
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, onlyPKpredictions_train)
+    print("accuracy BRFC AUC:",metrics.auc(fpr,tpr))
+
+    ###########################################################
+
+    X_train, y_train = SMOTE().fit_resample(trainData, trainLabel)
+ 
+    clf_test = BalancedRandomForestClassifier(n_estimators=100, max_depth=5)
+    clf_test = clf_test.fit(X_train, y_train)
+
+    y_dt_pred_test = clf_test.predict_proba(testData)
+
+    onlyPKpredictions_test = y_dt_pred_test[:,1]
+
+    ###########################################################
+
+    o = open('features103_pred3.txt', 'w')
+    for i in range(len(extractIds)):
+        entry = extractIds[i] + "\t" + str(onlyPKpredictions_test[i])
+        o.write(entry)
+        o.write('\n')
+
+    o.close()
+
+if __name__ == "__main__":
+    main()
--- sklearn_experimental.py
+++ sklearn_experimental.py
@@ -6,13 +6,15 @@ from sklearn import model_selection
 from sklearn import svm
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.over_sampling import SMOTE 
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.ensemble import BalancedRandomForestClassifier 
 
 # 2995  - pk
 # 24362 - pkf
 # 1:8 ratio
 
 def main():
-    f = open("trainingData/feature103_Train.txt")
+    f = open("trainingData/featuresall_train.txt")
 
     data = []
     floatData = []
@@ -43,7 +45,7 @@ def main():
     classLabel = np.array(label)
     trainData = np.array(data)
 
-    ros = RandomOverSampler(random_state=0)
+    # ros = RandomOverSampler(random_state=0)
 
     # X_resampled, y_resampled = SMOTE().fit_resample(trainData, classLabel)
 
@@ -53,26 +55,62 @@ def main():
     # X_train, y_train = ros.fit_resample(X_train, y_train)
     X_train, y_train = SMOTE().fit_resample(X_train, y_train)
 
+    # Gaussian Naive Bayes SUCKS don't use it
     nb = GaussianNB()
     nb.fit(X_train, y_train)
     y_nb_pred = nb.predict_proba(X_test)
     # print(y_nb_pred)
+    onlyPKpredictions = y_nb_pred[:,1]
+    # print(onlyPKpredictions)
     # print("accuracy KAPPA:",metrics.cohen_kappa_score(y_test, y_nb_pred))
-    print("accuracy AUC:",metrics.auc(y_test, y_nb_pred))
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, onlyPKpredictions)
+    print("accuracy NB AUC:",metrics.auc(fpr,tpr))
     # print("confusion:\n",metrics.confusion_matrix(y_test, y_nb_pred))
 
-    clf = tree.DecisionTreeClassifier(criterion='entropy')
+    # We are blessed with DT
+    clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=5)#, max_depth=20)
     # clf = tree.DecisionTreeRegressor()
     clf = clf.fit(X_train, y_train)
 
-    y_dt_pred = clf.predict(X_test)
-    # print(y_dt_pred[0])
-    print("accuracy KAPPA:",metrics.cohen_kappa_score(y_test, y_dt_pred))
+    y_dt_pred = clf.predict_proba(X_test)
+    # print(y_dt_pred[:,0])
+
+    onlyPKpredictions = y_dt_pred[:,1]
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, onlyPKpredictions)
+    print("accuracy DT AUC:",metrics.auc(fpr,tpr))
+    # print(y_dt_pred)
+
+    # print("accuracy KAPPA:",metrics.cohen_kappa_score(y_test, y_dt_pred))
+    # print("accuracy AUC:",metrics.roc_auc_score(y_test, y_dt_pred))
     # print("DT:\n",metrics.confusion_matrix(y_test, y_dt_pred))
 
-    model = svm.SVC(gamma='auto', kernel='linear')
-    y_svc_pred = model.fit(X_train, y_train)
-    print("accuracy KAPPA:",metrics.cohen_kappa_score(y_test, y_svc_pred))
+    model = BalancedRandomForestClassifier(n_estimators=100, max_depth=5)
+    model = model.fit(X_train, y_train)
+    y_rfc_pred = model.predict_proba(X_test)
+    onlyPKpredictions = y_rfc_pred[:,1]
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, onlyPKpredictions)
+    print("accuracy RFC AUC:",metrics.auc(fpr,tpr))
+    # print(y_rfc_pred)
+    # print("accuracy AUC:",metrics.roc_auc_score(y_test, y_rfc_pred))
+
+    f3 = open('results_train103.txt')
+    grab = []
+    for line in f3:
+        spl = line.split('\t')
+        grab.append(float(spl[1]))
+
+    nn = np.array(grab) 
+
+    fpr, tpr, thresholds = metrics.roc_curve(classLabel, nn)
+    print("accuracy NN AUC:",metrics.auc(fpr,tpr))
+
+
+    f3.close()
+
+    # SVM take way too long...why? Too large? DT went very fast though
+    # model = svm.SVC(gamma='auto')
+    # y_svc_pred = model.fit(X_train, y_train)
+    # print("accuracy KAPPA:",metrics.cohen_kappa_score(y_test, y_svc_pred))
 
 
 if __name__ == "__main__":
